9cda1b17b3188281aeb7a0f03ea325697f2a1c1b
Daemonicc <40716797+Daemonicc@users.noreply.github.com>
2019-02-17 01:11:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use(authRoute);
 app.use(campgroundRoute);
 app.use(commentRoute);
 
-app.listen(8080, function(){
-    console.log('server started');
-})
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
